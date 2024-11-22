1723c9299a77f08469c642a92c24593b68d018f4
Sayantika Pal <pal.sayan@northeastern.edu>
2024-11-22 15:47:16 -0500

    Committed the JavaFX folder structure

--- src/main/java/csye6200/app/Main.java
+++ src/main/java/csye6200/app/Main.java
@@ -1,7 +1,9 @@
-package application;
+package main.java.csye6200.app;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
@@ -10,9 +12,10 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
+			Parent root = FXMLLoader.load(getClass().getResource("/main/resources/fxml/test.fxml"));
+//			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			scene.getStylesheets().add(getClass().getResource("/main/resources/css/application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
