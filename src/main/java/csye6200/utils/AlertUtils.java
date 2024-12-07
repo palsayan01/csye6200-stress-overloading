b2c572f5fbcf177e121c6704f0c21a759fc6c9f0
Harshith Patil <harshithptl@gmail.com>
2024-12-07 01:37:28 -0500

    Missing file

---
+++ src/main/java/csye6200/utils/AlertUtils.java
@@ -0,0 +1,29 @@
+package main.java.csye6200.utils;
+
+import javafx.application.Platform;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.stage.Stage;
+
+public class AlertUtils {
+
+	public static void showAlert(Alert.AlertType alertType, String message, Stage owner) {
+		Platform.runLater(() -> {
+			Alert alert = new Alert(alertType, message, ButtonType.OK);
+			
+			if (owner != null) {
+	            alert.initOwner(owner);
+	        }
+			
+			alert.setTitle(alertType == Alert.AlertType.ERROR ? "Error" : "Information");
+	
+		    // Prevent interactions with the parent window while alert is active
+		    Stage alertStage = (Stage) alert.getDialogPane().getScene().getWindow();
+		    alertStage.setAlwaysOnTop(true);
+		    alertStage.setResizable(false);
+	
+		    // Block until the user responds to the alert
+		    alert.show();
+			});
+	}
+}
