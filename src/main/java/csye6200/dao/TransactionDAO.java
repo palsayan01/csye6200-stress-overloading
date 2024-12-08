1bdc6c03da5655aefdc71be3ef71c6472a1ae67e
Prathamesh Shukla <79475157+prathamexh@users.noreply.github.com>
2024-12-07 20:08:26 -0500

    fixed date issue

--- src/main/java/csye6200/dao/TransactionDAO.java
+++ src/main/java/csye6200/dao/TransactionDAO.java
@@ -14,13 +14,14 @@ public class TransactionDAO {
     }
 
     public boolean addTransaction(Transaction transaction) {
-        String sql = "INSERT INTO transactions (id, description, amount, transaction_date, category_id, transaction_type, userid) VALUES (?,?, ?, TO_DATE(?, 'YY-MM-DD'), ?, ?, ?)";
+        String sql = "INSERT INTO transactions (id, description, amount, transaction_date, category_id, transaction_type, userid) VALUES (?,?, ?, ?, ?, ?, ?)";
         
         try (PreparedStatement stmt = connection.prepareStatement(sql)) {
         	stmt.setString(1,transaction.getId());
             stmt.setString(2, transaction.getDescription());
             stmt.setDouble(3, transaction.getAmount());
-            stmt.setDate(4, Date.valueOf(transaction.getDate()));
+            System.out.println(transaction.getDate());
+            stmt.setDate(4, java.sql.Date.valueOf(transaction.getDate()));
             stmt.setString(5, transaction.getCategory());  // Category is now a String
             stmt.setString(6, transaction.getType().toString());
             stmt.setString(7,  transaction.getUserid());
@@ -34,11 +35,12 @@ public class TransactionDAO {
     }
 
     public boolean updateTransaction(Transaction transaction) throws SQLException {
-        String query = "UPDATE transactions SET description = ?, amount = ?, category_id = ?, transaction_date = TO_DATE(?, 'YY-MM-DD'), transaction_type = ? WHERE id = ?";
+        String query = "UPDATE transactions SET description = ?, amount = ?, category_id = ?, transaction_date = ?, transaction_type = ? WHERE id = ?";
         try (PreparedStatement stmt = connection.prepareStatement(query)) {
             stmt.setString(1, transaction.getDescription());
             stmt.setDouble(2, transaction.getAmount());
             stmt.setString(3, transaction.getCategory());
+//            stmt.setDate(4, java.sql.Date.valueOf(transaction.getDate()));
             stmt.setDate(4, java.sql.Date.valueOf(transaction.getDate()));
             stmt.setString(5, transaction.getType().name());
             stmt.setString(6, transaction.getId());
